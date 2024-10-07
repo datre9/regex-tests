d7fed1012ea43f55d91d59d547d875739175e0d5
datre9 <david.trembacz@gmail.com>
2024-10-07 22:08:16 +0200

    Improve java demo

--- java-demo/src/Main.java
+++ java-demo/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -5,21 +6,28 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) throws IOException {
         String string = GetStringFromFile.getStringFromFile("../adventures-of-huckleberry-finn.txt");
-
         Runtime runtime = Runtime.getRuntime();
+        StringBuilder toWrite = new StringBuilder("id;time;memory\n");
 
-        long startMemory = runtime.totalMemory() - runtime.freeMemory();
-        long startTime = System.nanoTime();
+        //number of test repeats
+        for (int i = 0; i < 1000; i++) {
+            long startMemory = runtime.totalMemory() - runtime.freeMemory();
+            long startTime = System.nanoTime();
 
-        Pattern pattern = Pattern.compile("finn|huckleberry", Pattern.CASE_INSENSITIVE);
-        for (int i = 0; i < 10000; i++) {
-            Matcher matcher = pattern.matcher(string);
+            Pattern pattern = Pattern.compile("finn|huckleberry", Pattern.CASE_INSENSITIVE);
+            //does not measure difference in memory for smaller numbers
+            for (int j = 0; j < 10000; j++) {
+                Matcher matcher = pattern.matcher(string);
+            }
+
+            long endTime = System.nanoTime();
+            long endMemory = runtime.totalMemory() - runtime.freeMemory();
+            //sometimes throws a negative number or a zero for memory
+            if (endMemory - startMemory > 0) toWrite.append(i).append(";").append(endTime - startTime).append(";").append(endMemory - startMemory).append("\n");
         }
 
-        long endTime = System.nanoTime();
-        long endMemory = runtime.totalMemory() - runtime.freeMemory();
-
-        System.out.println(endTime - startTime + " ns");
-        System.out.println(endMemory - startMemory + " B");
+        FileWriter myWriter = new FileWriter("results.csv");
+        myWriter.write(toWrite.toString());
+        myWriter.close();
     }
 }
