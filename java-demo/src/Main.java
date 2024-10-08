0cb1e32085ef7fcd31faaf4099635e8100fb84d2
datre9 <david.trembacz@gmail.com>
2024-10-08 20:10:19 +0200

    Fix java demo test

--- java-demo/src/Main.java
+++ java-demo/src/Main.java
@@ -6,24 +6,26 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) throws IOException {
         String string = GetStringFromFile.getStringFromFile("../adventures-of-huckleberry-finn.txt");
+        StringBuilder toWrite = new StringBuilder("id;time(ns);memory(B)\n");
+        //used for getting current RAM usage
         Runtime runtime = Runtime.getRuntime();
-        StringBuilder toWrite = new StringBuilder("id;time;memory\n");
+
+        Pattern pattern = Pattern.compile("finn|huckleberry", Pattern.CASE_INSENSITIVE);
 
         //number of test repeats
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
+            //garbage collector to avoid wierd data
+            runtime.gc();
             long startMemory = runtime.totalMemory() - runtime.freeMemory();
             long startTime = System.nanoTime();
 
-            Pattern pattern = Pattern.compile("finn|huckleberry", Pattern.CASE_INSENSITIVE);
-            //does not measure difference in memory for smaller numbers
-            for (int j = 0; j < 10000; j++) {
-                Matcher matcher = pattern.matcher(string);
-            }
+            Matcher matcher = pattern.matcher(string);
+            matcher.matches();
 
             long endTime = System.nanoTime();
             long endMemory = runtime.totalMemory() - runtime.freeMemory();
-            //sometimes throws a negative number or a zero for memory
-            if (endMemory - startMemory > 0) toWrite.append(i).append(";").append(endTime - startTime).append(";").append(endMemory - startMemory).append("\n");
+
+            toWrite.append(i).append(";").append(endTime - startTime).append(";").append(endMemory - startMemory).append("\n");
         }
 
         FileWriter myWriter = new FileWriter("results.csv");
