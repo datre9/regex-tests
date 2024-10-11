77579dbef44a93ec0e4b234afc09e034c2280e00
datre9 <david.trembacz@gmail.com>
2024-10-11 13:57:50 +0200

    WIP2 c# demo

--- c#-demo/Program.cs
+++ c#-demo/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
 
 
@@ -9,23 +8,19 @@ sr.Close();
 
 StringBuilder toWrite = new StringBuilder("id;time(ns);memory(B)\n");
 
-//Process process = Process.GetCurrentProcess();
-
 Regex.CacheSize = 0;
 Regex regex = new Regex("finn|huckleberry", RegexOptions.IgnoreCase);
 
-for (int i = 0; i < 100; i++) {
-	GC.Collect();
-	GC.WaitForPendingFinalizers();
-	long startMemory = GC.GetTotalMemory(true);
-	long startTime = DateTime.Now.Ticks;
+GC.Collect();
+GC.WaitForPendingFinalizers();
+long startMemory = GC.GetTotalMemory(true);
+long startTime = DateTime.Now.Ticks;
 
-	regex.Matches(s);
+regex.Matches(s);
 
-	long endTime = DateTime.Now.Ticks;
-	long endMemory = GC.GetTotalMemory(true);
+long endTime = DateTime.Now.Ticks;
+long endMemory = GC.GetTotalMemory(true);
 
-	toWrite.Append(i).Append(";").Append((endTime - startTime) * 100).Append(";").Append(endMemory - startMemory).Append("\n");
-}
+toWrite.Append(0).Append(";").Append((endTime - startTime) * 100).Append(";").Append(endMemory - startMemory).Append("\n");
 
 Console.WriteLine(toWrite);
