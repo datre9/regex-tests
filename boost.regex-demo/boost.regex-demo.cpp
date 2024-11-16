af53ff0a8fa025ce1cc9cc635a81736478b04e0a
datre9 <david.trembacz@gmail.com>
2024-11-16 11:14:52 +0100

    Implement boost.regex

--- boost.regex-demo/boost.regex-demo.cpp
+++ boost.regex-demo/boost.regex-demo.cpp
@@ -5,8 +5,37 @@
 #include <boost/regex.hpp>
 #include <chrono>
 
+using namespace std;
+
 int main()
 {
+    ifstream f("../adventures-of-huckleberry-finn.txt");
+    string s;
+    ostringstream ss;
+    ss << f.rdbuf();
+    s = ss.str();
+    f.close();
+    boost::regex pattern("Finn|Huckleberry");
+
+    stringstream toWrite;
+    toWrite << "id;time(ns);memory(B)\n";
+
+    for (int i = 0; i < 1000; i++) {
+        auto start = chrono::high_resolution_clock::now();
+
+        boost::sregex_iterator iter(s.begin(), s.end(), pattern);
+        boost::sregex_iterator end;
+        for (; iter != end; ++iter) { }
+
+        auto finish = chrono::high_resolution_clock::now();
+
+        auto elapsed = chrono::duration_cast<chrono::nanoseconds>(finish - start).count();
+        toWrite << i << ";" << elapsed << ";" << 0 << "\n";
+    }
+
+    ofstream f2("results.csv");
+    f2 << toWrite.str();
+    f2.close();
 
     return 0;
 }
