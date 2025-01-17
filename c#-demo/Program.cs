8880a6b4eb71e40a2ac71f1db6d34ec8f027541a
datre9 <david.trembacz@gmail.com>
2025-01-17 14:09:58 +0100

    Modular c#

--- c#-demo/Program.cs
+++ c#-demo/Program.cs
@@ -1,31 +1,48 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Text.RegularExpressions;
 
 
-StreamReader sr = new StreamReader("../../../../adventures-of-huckleberry-finn.txt");
-string s = sr.ReadToEnd();
-sr.Close();
+class Program {
+	static StringBuilder toWrite = new StringBuilder("");
 
-StringBuilder toWrite = new StringBuilder("id;time(ns);memory(B)\n");
+	static void Main(string[] args) {
+		StreamReader sr = new StreamReader("../../../../abc1.txt");
+		string abc1 = sr.ReadToEnd();
+		sr = new StreamReader("../../../../abc2.txt");
+		string abc2 = sr.ReadToEnd();
+		sr = new StreamReader("../../../../abc3.txt");
+		string abc3 = sr.ReadToEnd();
+		sr.Close();
 
-Regex regex = new Regex("Finn|Huckleberry", 0);
+		Regex regexKMP = new Regex("(ab)+", RegexOptions.Compiled);
 
-for (int i = 0; i < 1000; i++) {
-	Match match = regex.Match(s);
 
-	long startMemory = GC.GetTotalMemory(true);
-	long startTime = DateTime.Now.Ticks;
+		Benchmark(abc1, regexKMP, "KMP1");
+		Benchmark(abc2, regexKMP, "KMP2");
+		Benchmark(abc3, regexKMP, "KMP3");
 
-	while (match.Success) {
-		match = match.NextMatch();
+
+		StreamWriter sw = new StreamWriter("../../../results.csv");
+		sw.WriteLine(toWrite);
+		sw.Close();
 	}
 
-	long endTime = DateTime.Now.Ticks;
-	long endMemory = GC.GetTotalMemory(true);
+	static void Benchmark(string text, Regex regex, string title) {
+		toWrite.Append(title).Append("\n");
 
-	toWrite.Append(i).Append(";").Append((endTime - startTime) * 100).Append(";").Append(endMemory - startMemory).Append("\n");
+		for (int i = 0; i < 1000; i++) {
+			Match match = regex.Match(text);
+
+			Stopwatch sw = Stopwatch.StartNew();
+
+			while (match.Success) {
+				match = match.NextMatch();
+			}
+
+			sw.Stop();
+
+			toWrite.Append(sw.ElapsedTicks * 100).Append("\n");
+		}
+	}
 }
-
-StreamWriter sw = new StreamWriter("../../../results.csv");
-sw.WriteLine(toWrite);
-sw.Close();
