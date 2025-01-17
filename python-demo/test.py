90c835b060681a85ff197d71a909afc3721e9975
datre9 <david.trembacz@gmail.com>
2025-01-17 19:26:35 +0100

    Modular python

--- python-demo/test.py
+++ python-demo/test.py
@@ -1,27 +1,40 @@
 import os
 import re
 import time
-import tracemalloc
 
-file_path = os.path.join(os.path.dirname(__file__), "../adventures-of-huckleberry-finn.txt")
+toWrite = ""
+
+def benchmark(text, regex, title):
+    global toWrite
+    toWrite += f"{title}\n"
+    
+    for _ in range(1000):
+        startTime = time.time_ns()
+        
+        re.findall(regex, text)
+        
+        endTime = time.time_ns()
+        
+        toWrite += f"{endTime - startTime}\n"
+
+
+file_path = os.path.join(os.path.dirname(__file__), "../abc1.txt")
 with open(file_path, "r", encoding="utf-8") as file:
-    string = file.read()
+    abc1 = file.read()
+file_path = os.path.join(os.path.dirname(__file__), "../abc2.txt")
+with open(file_path, "r", encoding="utf-8") as file:
+    abc2 = file.read()
+file_path = os.path.join(os.path.dirname(__file__), "../abc3.txt")
+with open(file_path, "r", encoding="utf-8") as file:
+    abc3 = file.read()
 
-toWrite = "id;time(ns);memory(B)\n"
-regex = re.compile("Finn|Huckleberry", 0)
+regexKMP = re.compile("(ab)+", 0)
 
-for i in range(1000):
-    tracemalloc.start()
-    startTime = time.time_ns()
-    
-    re.findall(regex, string)
-    
-    endTime = time.time_ns()
-    endMemory = tracemalloc.get_traced_memory()
-    tracemalloc.stop()
-    tracemalloc.reset_peak()
-    
-    toWrite += f"{i};{endTime - startTime};{endMemory[1]}\n"
+
+benchmark(abc1, regexKMP, "KMP1")
+benchmark(abc2, regexKMP, "KMP2")
+benchmark(abc3, regexKMP, "KMP3")
+
 
 output_path = os.path.join(os.path.dirname(__file__), "results.csv")
 with open(output_path, "w") as output:
