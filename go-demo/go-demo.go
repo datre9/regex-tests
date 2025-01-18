a6201558b64d46224392dea7be5ee7e893d73806
datre9 <david.trembacz@gmail.com>
2025-01-18 19:29:03 +0100

    Modular go

--- go-demo/go-demo.go
+++ go-demo/go-demo.go
@@ -8,23 +8,40 @@ import (
 	"time"
 )
 
+var toWrite bytes.Buffer
+
 func main() {
-	s, err := os.ReadFile("../adventures-of-huckleberry-finn.txt")
+	abc1, err := os.ReadFile("../abc1.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
-	var toWrite bytes.Buffer
-	toWrite.WriteString("id;time(ns);memory(B)\n")
-	regex := regexp.MustCompile("Finn|Huckleberry")
+	abc2, err := os.ReadFile("../abc2.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	abc3, err := os.ReadFile("../abc3.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	regexKMP := regexp.MustCompile("(ab)+")
+
+	benchmark(abc1, regexKMP, "KMP1")
+	benchmark(abc2, regexKMP, "KMP2")
+	benchmark(abc3, regexKMP, "KMP3")
+
+	os.WriteFile("results.csv", toWrite.Bytes(), 0644)
+}
+
+func benchmark(text []byte, regex *regexp.Regexp, title string) {
+	toWrite.WriteString(fmt.Sprintf("%s\n", title))
 
 	for i := 0; i < 1000; i++ {
 		start := time.Now()
 
-		regex.FindAll(s, -1)
+		regex.FindAll(text, -1)
 
 		elapsed := time.Since(start)
-		toWrite.WriteString(fmt.Sprintf("%d;%d;0\n", i, elapsed.Nanoseconds()))
+		toWrite.WriteString(fmt.Sprintf("%d\n", elapsed.Nanoseconds()))
 	}
-
-	os.WriteFile("results.csv", toWrite.Bytes(), 0644)
 }
