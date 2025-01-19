c2c4838204f6bd03ef1b3f33fafedbe84371fad8
datre9 <david.trembacz@gmail.com>
2025-01-19 13:45:28 +0100

    Modular re2

--- re2-demo/re2-demo.cpp
+++ re2-demo/re2-demo.cpp
@@ -7,34 +7,45 @@
 
 using namespace std;
 
-int main() {
-    ifstream f("../adventures-of-huckleberry-finn.txt");
-    string s;
-    ostringstream ss;
-    ss << f.rdbuf();
-    s = ss.str();
-    f.close();
-    string pattern = R"((Finn|Huckleberry))";
+stringstream toWrite;
 
-    stringstream toWrite;
-    toWrite << "id;time(ns);memory(B)\n";
-
-    RE2 re(pattern);
-    re2::StringPiece input(s);
+static void benchmark(const string& text, const RE2& regex, const string& title) {
+    toWrite << title << "\n";
 
     for (int i = 0; i < 1000; i++) {
-        re2::StringPiece input(s);
+        re2::StringPiece input(text);
 
         auto start = chrono::high_resolution_clock::now();
 
-        while (RE2::FindAndConsume(&input, re)) {
+        while (RE2::FindAndConsume(&input, regex)) {
         }
 
         auto finish = chrono::high_resolution_clock::now();
 
         auto elapsed = chrono::duration_cast<chrono::nanoseconds>(finish - start).count();
-        toWrite << i << ";" << elapsed << ";" << 0 << "\n";
+        toWrite << elapsed << "\n";
     }
+}
+
+string readFile(const std::string& path) {
+    std::ifstream f(path);
+    std::ostringstream ss;
+    ss << f.rdbuf();
+    return ss.str();
+}
+
+int main() {
+    string abc1 = readFile("../abc1.txt");
+    string abc2 = readFile("../abc2.txt");
+    string abc3 = readFile("../abc3.txt");
+
+    RE2 regexKMP("Finn|Huckleberry");
+
+
+    benchmark(abc1, regexKMP, "KMP1");
+    benchmark(abc2, regexKMP, "KMP2");
+    benchmark(abc3, regexKMP, "KMP3");
+
 
     ofstream f2("results.csv");
     f2 << toWrite.str();
